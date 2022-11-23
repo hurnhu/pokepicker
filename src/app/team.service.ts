aa28597c73c1e97a73259353a51bf7ae621065af
Michael lapan <codethrower@gmail.com>
2022-11-23 18:09:20 -0500

    working

--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +14,7 @@ export class TeamService {
     this.loadTeams()
   }
 
-  private teams = new Subject<any>;
+  private teams = new BehaviorSubject({});
   
   getTeams(): Observable<any> {
     return this.teams.asObservable();
